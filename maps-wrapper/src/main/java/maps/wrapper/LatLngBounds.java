3b0992e6999b834f7a085ccc75372c52349684df
Adi <adrian.dan.busuioc@gmail.com>
2020-10-01 13:32:49 +0200

    Add LatLngBounds#contains and LatLngBounds#getCenter.

--- maps-wrapper/src/main/java/maps/wrapper/LatLngBounds.java
+++ maps-wrapper/src/main/java/maps/wrapper/LatLngBounds.java
@@ -21,6 +21,28 @@ public class LatLngBounds {
         return new LatLngBounds.Builder();
     }
 
+    public final boolean contains(LatLng point) {
+        if (google != null) {
+            return google.contains(new com.google.android.gms.maps.model.LatLng(point.latitude, point.longitude));
+        } else if (huawei != null) {
+            return huawei.contains(new com.huawei.hms.maps.model.LatLng(point.latitude, point.longitude));
+        } else {
+            throw new UnsupportedOperationException("Missing underlying GMS/HMS LatLngBounds.");
+        }
+    }
+
+    public final LatLng getCenter() {
+        if (google != null) {
+            final com.google.android.gms.maps.model.LatLng center = google.getCenter();
+            return new LatLng(center.latitude, center.longitude);
+        } else if (huawei != null) {
+            final com.huawei.hms.maps.model.LatLng center = huawei.getCenter();
+            return new LatLng(center.latitude, center.longitude);
+        } else {
+            throw new UnsupportedOperationException("Missing underlying GMS/HMS LatLngBounds.");
+        }
+    }
+
     public static class Builder {
         private com.google.android.gms.maps.model.LatLngBounds.Builder google;
         private com.huawei.hms.maps.model.LatLngBounds.Builder huawei;
