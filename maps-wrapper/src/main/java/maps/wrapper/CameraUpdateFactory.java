be7ddc0e989c90e08570ab0b86c104bc490f21b0
Adi <adrian.dan.busuioc@gmail.com>
2020-10-01 13:21:51 +0200

    Add zoomBy to CameraUpdateFactory.

--- maps-wrapper/src/main/java/maps/wrapper/CameraUpdateFactory.java
+++ maps-wrapper/src/main/java/maps/wrapper/CameraUpdateFactory.java
@@ -1,135 +1,172 @@
 package maps.wrapper;
 
+import android.graphics.Camera;
+import android.graphics.Point;
+
 public class CameraUpdateFactory {
 
-    public static CameraUpdate zoomIn(){
+    public static CameraUpdate zoomIn() {
         com.google.android.gms.maps.CameraUpdate google = null;
         com.huawei.hms.maps.CameraUpdate huawei = null;
-        try{
+        try {
             google = com.google.android.gms.maps.CameraUpdateFactory.zoomIn();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        try{
+        try {
             huawei = com.huawei.hms.maps.CameraUpdateFactory.zoomIn();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return new CameraUpdate(huawei, google);
     }
 
-    public static CameraUpdate zoomOut(){
+    public static CameraUpdate zoomOut() {
         com.google.android.gms.maps.CameraUpdate google = null;
         com.huawei.hms.maps.CameraUpdate huawei = null;
-        try{
+        try {
             google = com.google.android.gms.maps.CameraUpdateFactory.zoomOut();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        try{
+        try {
             huawei = com.huawei.hms.maps.CameraUpdateFactory.zoomOut();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return new CameraUpdate(huawei, google);
     }
 
 
-    public static CameraUpdate scrollBy(float x, float y){
+    public static CameraUpdate scrollBy(float x, float y) {
         com.google.android.gms.maps.CameraUpdate google = null;
         com.huawei.hms.maps.CameraUpdate huawei = null;
-        try{
-            google = com.google.android.gms.maps.CameraUpdateFactory.scrollBy(x,y);
-        }catch(Exception e){
+        try {
+            google = com.google.android.gms.maps.CameraUpdateFactory.scrollBy(x, y);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        try{
-            huawei = com.huawei.hms.maps.CameraUpdateFactory.scrollBy(x,y);
-        }catch(Exception e){
+        try {
+            huawei = com.huawei.hms.maps.CameraUpdateFactory.scrollBy(x, y);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return new CameraUpdate(huawei, google);
     }
 
-    public static CameraUpdate zoomTo(float value){
+    public static CameraUpdate zoomTo(float value) {
         com.google.android.gms.maps.CameraUpdate google = null;
         com.huawei.hms.maps.CameraUpdate huawei = null;
-        try{
+        try {
             google = com.google.android.gms.maps.CameraUpdateFactory.zoomTo(value);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        try{
+        try {
             huawei = com.huawei.hms.maps.CameraUpdateFactory.zoomTo(value);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return new CameraUpdate(huawei, google);
     }
 
-    public static CameraUpdate newLatLngZoom(LatLng position, float zoom){
+    public static CameraUpdate zoomBy(float value) {
         com.google.android.gms.maps.CameraUpdate google = null;
         com.huawei.hms.maps.CameraUpdate huawei = null;
-        try{
-             google = com.google.android.gms.maps.CameraUpdateFactory.newLatLngZoom(position.google, zoom);
-        }catch(Exception e){}
+        try {
+            google = com.google.android.gms.maps.CameraUpdateFactory.zoomBy(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            huawei = com.huawei.hms.maps.CameraUpdateFactory.zoomBy(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        try{
+        return new CameraUpdate(huawei, google);
+    }
+
+    public static CameraUpdate zoomBy(float value, Point point) {
+        com.google.android.gms.maps.CameraUpdate google = null;
+        com.huawei.hms.maps.CameraUpdate huawei = null;
+        try {
+            google = com.google.android.gms.maps.CameraUpdateFactory.zoomBy(value, point);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            huawei = com.huawei.hms.maps.CameraUpdateFactory.zoomBy(value, point);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new CameraUpdate(huawei, google);
+    }
+
+    public static CameraUpdate newLatLngZoom(LatLng position, float zoom) {
+        com.google.android.gms.maps.CameraUpdate google = null;
+        com.huawei.hms.maps.CameraUpdate huawei = null;
+        try {
+            google = com.google.android.gms.maps.CameraUpdateFactory.newLatLngZoom(position.google, zoom);
+        } catch (Exception e) {
+        }
+
+        try {
             huawei = com.huawei.hms.maps.CameraUpdateFactory.newLatLngZoom(position.huawei, zoom);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return new CameraUpdate(huawei, google);
     }
 
-    public static CameraUpdate newCameraPosition(CameraPosition position){
+    public static CameraUpdate newCameraPosition(CameraPosition position) {
         com.google.android.gms.maps.CameraUpdate google = null;
         com.huawei.hms.maps.CameraUpdate huawei = null;
-        try{
+        try {
             google = com.google.android.gms.maps.CameraUpdateFactory.newCameraPosition(position.google);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        try{
+        try {
             huawei = com.huawei.hms.maps.CameraUpdateFactory.newCameraPosition(position.huawei);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return new CameraUpdate(huawei, google);
     }
 
-    public static CameraUpdate newLatLngBounds(LatLngBounds bounds, int value){
+    public static CameraUpdate newLatLngBounds(LatLngBounds bounds, int value) {
         com.google.android.gms.maps.CameraUpdate google = null;
         com.huawei.hms.maps.CameraUpdate huawei = null;
-        try{
+        try {
             google = com.google.android.gms.maps.CameraUpdateFactory.newLatLngBounds(bounds.google, value);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        try{
+        try {
             huawei = com.huawei.hms.maps.CameraUpdateFactory.newLatLngBounds(bounds.huawei, value);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return new CameraUpdate(huawei, google);
     }
 
-    public static CameraUpdate newLatLng(LatLng position){
+    public static CameraUpdate newLatLng(LatLng position) {
         com.google.android.gms.maps.CameraUpdate google = null;
         com.huawei.hms.maps.CameraUpdate huawei = null;
-        try{
+        try {
             google = com.google.android.gms.maps.CameraUpdateFactory.newLatLng(position.google);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        try{
+        try {
             huawei = com.huawei.hms.maps.CameraUpdateFactory.newLatLng(position.huawei);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return new CameraUpdate(huawei, google);
     }
-
 }
