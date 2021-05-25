ddb71427ced030392a485fe6685eabb7c1703808
Stefan Schweizer <sschweizer@cellular.de>
2021-05-25 16:20:36 +0200

    Fix latitude and longitude in LatLng
    LatLng's constructors in the GMS and HMS implementations might change
    the values for latitude and longitude but the wrapper library was always
    using the original values passed to the constructor resulting in
    different values in the mapper library and the maps implementations.
    
    This fix uses the updated values of lat/lng from the GMS
    implementation. The GMS and HMS implementations of LatLng are equivalent
    and both wrapped objects should always be present.

--- maps-wrapper/src/main/java/maps/wrapper/LatLng.java
+++ maps-wrapper/src/main/java/maps/wrapper/LatLng.java
@@ -10,7 +10,8 @@ public class LatLng {
     public LatLng(double latitude, double longitude) {
         this.huawei = new com.huawei.hms.maps.model.LatLng(latitude, longitude);
         this.google = new com.google.android.gms.maps.model.LatLng(latitude, longitude);
-        this.latitude = latitude;
-        this.longitude = longitude;
+        // LatLng's constructor might change lat/lng
+        this.latitude = google.latitude;
+        this.longitude = google.longitude;
     }
 }
